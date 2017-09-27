e36e783e8677ab2ffcf8dd151f33322c08e0dcae
Lukas Musil <lmusil@redhat.com>
2017-09-27 23:02:07 +0200

    Initial emacs commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,34 @@
+;; init.el --- Emacs configuration
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-config-default nil)
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(global-auto-complete-mode t)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (python py-autopep8 python-mode magit auto-complete)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "xos4 Terminus" :foundry "xos4" :slant normal :weight normal :height 136 :width normal)))))
+
+ (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
